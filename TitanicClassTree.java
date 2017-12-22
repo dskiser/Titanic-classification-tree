d8aab8036f91dad9e657ef34afb0f70f9f5d53a0
Daniel Kiser <dskiser@email.uark.edu>
2017-12-22 11:14:48 -0600

    Removed Observation class and stored data in two-dimensional matrix rather than as Observation objects

--- TitanicClassTree.java
+++ TitanicClassTree.java
@@ -1,94 +1,48 @@
 import java.io.*;
 
-// Observation object
-class Observation {
-    private int id, survived, pclass, sibsp, parch;
-    private String sex, ticket, cabin, embark;
-    private double fare, age;
-
-    Observation(String[] array) {
-        String[] variables = array;
-        
-        if(variables[0].compareTo("") != 0)
-            id = Integer.parseInt(variables[0]);
-        if(variables[1].compareTo("") != 0)
-            survived = Integer.parseInt(variables[1]);
-        if(variables[2].compareTo("") != 0)
-            pclass = Integer.parseInt(variables[2]);
-        sex = variables[5];
-        if(variables[6].compareTo("") != 0)
-            age = Double.parseDouble(variables[6]);
-        if(variables[7].compareTo("") != 0)
-            sibsp = Integer.parseInt(variables[7]);
-        if(variables[8].compareTo("") != 0)
-        parch = Integer.parseInt(variables[8]);
-        ticket = variables[9];
-        if(variables[10].compareTo("") != 0)
-            fare = Double.parseDouble(variables[10]);
-        cabin = variables[11];
-        embark = variables[12];
-    }
-
-    public void getObs() {
-        System.out.println(id + " " + survived + " " + pclass + " " + sex + " "
-                + age + " " + sibsp + " " + parch + " " + ticket + " " + fare +
-                " " + cabin + " " + embark);
-    }
-
-    public boolean survived() {
-        if(survived == 1) 
-            return true;
-        else
-            return false;
-    }
-}
-
 // Read CSV
 class ReadCSV {
-    public static Observation[] readCSV(String file, int observ) {
+    public static String[][] readCSV(String file, int lines, int variables) {
         String s;
         String file_name = file;
-        int num_observ = observ;
-        int count = 0;
+        int rows = lines;
+        int columns = variables;
+        String data[][] = new String[rows][columns];
 
-        Observation observations[] = new Observation[num_observ];
-
+        int row = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(file_name))){
             while((s = br.readLine()) != null) {
-                String[] variables = s.split(",", -1);
-                observations[count++] = new Observation(variables);
+                data[row++] = s.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
             }
         } catch(IOException exc) {
             System.out.println(exc);
         }
 
-        return observations;
+        return data;
 
     }
 }
 
 // Node object
 class Node {
-    private int survived, total;
+    private int survived, obsAtNode;
     private double p_survived, impurity;
     
-    Node(Observation[] array) {
-        Observation[] observations = array;
+    Node(String[][] data) {
+        String[][] node_data = data;
         
         survived = 0;
-        total = observations.length;
+        obsAtNode = data.length;
 
-        for(Observation x : observations) {
-            if(x.survived())
-                survived += 1;
-        }
+        for(int i=0; i<obsAtNode; i++)
+            survived += Integer.parseInt(data[i][1]);
 
-        p_survived = (double) survived / (double) total;
+        p_survived = (double) survived / (double) obsAtNode;
         impurity = 2 * p_survived * (1 - p_survived);
     } 
     
     public double getTotal() {
-        return total;
+        return obsAtNode;
     }
 
     public double getImpurity() {
@@ -99,28 +53,18 @@ class Node {
         return p_survived;
     }
 
-
-//    private ArrayList<Integer> getPossibleSplits(Observation array[], int column) {
-//        Observation[] node_obs = array;
-//        int var_num = column;
-//        ArrayList<Integer> uniq_vals = new ArrayList();
-//        
-//        for(Observation x : node_obs) {
-//            if(x
 }
-            
-            
-        
-
 
 // Main class
 class TitanicClassTree {
     public static void main(String args[]) {
-        Observation[] observations = ReadCSV.readCSV("train_wo_header.csv", 891);
+        String[][] data = ReadCSV.readCSV("train_wo_header.csv", 891, 12);
         
-        Node root = new Node(observations);
+        for(int i=0; i<12; i++)
+            System.out.println(data[4][i]); 
+
+        Node root = new Node(data);
         System.out.println("Total: " + root.getTotal());
-        System.out.println("Proportion Surviving: " + root.getPropSurv());
         System.out.println("Impurity: " + root.getImpurity());
     }
 }
