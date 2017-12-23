f6c17e945f2d69b21a08c5a1ee4db5cc02512e96
Daniel Kiser <dskiser@email.uark.edu>
2017-12-22 22:06:34 -0600

    Added constructor to create Split object if the value being split on is categorical.

--- TitanicClassTree.java
+++ TitanicClassTree.java
@@ -71,14 +71,15 @@ class Node {
 
 // Determines possible splits and best splits, and creates new nodes
 class Split {
-    double split_value, gini, impurity_after, pleft, pright;
+    double split_valueNum, gini, impurity_after, pleft, pright;
     int variable, num_left, num_right, countL, countR, leftORright;
     String[][] parent_data, nodeLdata, nodeRdata;
     Node parent, left, right;
+    String split_valueCat;
 
-    // Create a split object if value is number.
+    // Constructor for Split object if value is numerical.
     Split(double value, int column_num, String[][] data, Node parent_node) {
-        split_value = value;
+        split_valueNum = value;
         variable = column_num;
         parent_data = data;
         parent = parent_node;
@@ -103,7 +104,7 @@ class Split {
             }
             // If value is less than split value, observation goes to the left node.
             else if(Double.parseDouble(parent_data[i][variable])
-                    <= split_value)
+                    <= split_valueNum)
                 num_left += 1;
             // If value is greater than or equal to split value, observation goes
             // to the right node.
@@ -137,7 +138,7 @@ class Split {
             }
             // If value is less than split value, send observation to left node.
             else if(Double.parseDouble(parent_data[n][variable])
-                    <= split_value) {
+                    <= split_valueNum) {
               countL++;  
               for(int p=0; p<ReadCSV.columns; p++) {
                   nodeLdata[countL][p] = parent_data[n][p];
@@ -163,16 +164,94 @@ class Split {
         impurity_after = (pleft * left.getImpurity())
            + (pright * right.getImpurity()); 
         gini = parent.getImpurity() - impurity_after;
-
-        
     }
 
-    // Create a split object if value is category.
-    //Split(String value, int column_num) {
-    //    String split_value = value;
-    //    int variable = column_num;
-    //    double gini;
-    //}
+    // Constructor for Split object if value is categorical.
+    Split(String value, int column_num, String[][] data, Node parent_node) {
+        split_valueCat = value;
+        variable = column_num;
+        parent_data = data;
+        parent = parent_node;
+
+        // Calculate number of observations in left node and right node.
+        num_left = 0;
+        num_right = 0;
+        // Variable equals 0 or 1: 0 shunts blank values to left node, 1 shunts
+        // blank values to right node.
+        leftORright = 0;
+        for(int i=0; i<parent_data.length; i++) {
+            // If no value is available, alternate which node observation is sent to.
+            if(parent_data[i][variable].compareTo("") == 0) {
+                if(leftORright == 0) {
+                    num_left += 1;
+                    leftORright = 1;
+                }
+                else {
+                    num_right += 1;
+                    leftORright = 0;
+                }
+            }
+            // If value is same as split value, observation goes to the left node.
+            else if(parent_data[i][variable].compareTo(split_valueCat) == 0)
+                num_left += 1;
+            // If value different from split value, observation goes
+            // to the right node.
+            else
+                num_right += 1;
+        }
+
+        // Create datasets for left and right nodes.
+        nodeLdata = new String[num_left][ReadCSV.columns];
+        nodeRdata = new String[num_right][ReadCSV.columns];
+        countL = -1;
+        countR = -1;
+        leftORright = 0;
+        for(int n=0; n<parent_data.length; n++) {
+            // If no value is available, alternate which node observation is sent to.
+            if(parent_data[n][variable].compareTo("") == 0) {
+                if(leftORright == 0) {
+                    countL++;
+                    for(int p=0; p<ReadCSV.columns; p++) {
+                        nodeLdata[countL][p] = parent_data[n][p];
+                        leftORright = 1;
+                    }
+                }
+                else {
+                    countR++;
+                    for(int p=0; p<ReadCSV.columns; p++) { 
+                        nodeRdata[countR][p] = parent_data[n][p];
+                         leftORright = 0;
+                    }
+                }
+            }
+            // If value is same as split value, send observation to left node.
+            else if(parent_data[n][variable].compareTo(split_valueCat) == 0) {
+              countL++;  
+              for(int p=0; p<ReadCSV.columns; p++) {
+                  nodeLdata[countL][p] = parent_data[n][p];
+              }
+            }
+            // If value is different from split value, send observation
+            // to right node.
+            else {
+              countR++;
+              for(int p=0; p<ReadCSV.columns; p++) {
+                  nodeRdata[countR][p] = parent_data[n][p];
+              }
+            }
+        }
+
+        // Create new nodes.
+        left = new Node(nodeLdata);
+        right = new Node(nodeRdata);
+
+        // Calculate gini diversity index for split.
+        pleft = ((double) num_left / parent_data.length);
+        pright = ((double) num_right / parent_data.length);
+        impurity_after = (pleft * left.getImpurity())
+           + (pright * right.getImpurity()); 
+        gini = parent.getImpurity() - impurity_after;
+    }
 
     // Grab specified column from the data set
     private static String[] getColumn(String[][] data, int column_num) {
@@ -240,16 +319,27 @@ class TitanicClassTree {
 
         Node root = new Node(data);
 
-        Split testSplit = new Split(23.0, 5, data, root);
+        Split testSplitNum = new Split(23.0, 5, data, root);
         System.out.println("Parent node impurity: " + 
-                testSplit.parent.getImpurity());
+                testSplitNum.parent.getImpurity());
         System.out.println("Left node impurity: " + 
-                testSplit.left.getImpurity());
+                testSplitNum.left.getImpurity());
         System.out.println("Right node impurity: " + 
-                testSplit.right.getImpurity());
+                testSplitNum.right.getImpurity());
         System.out.println("Impurity after split: " + 
-                testSplit.impurity_after);
-        System.out.println("Gini: " + testSplit.gini);
+                testSplitNum.impurity_after);
+        System.out.println("Gini: " + testSplitNum.gini);
+
+        Split testSplitCat = new Split("1", 2, data, root);
+        System.out.println("Parent node impurity: " + 
+                testSplitCat.parent.getImpurity());
+        System.out.println("Left node impurity: " + 
+                testSplitCat.left.getImpurity());
+        System.out.println("Right node impurity: " + 
+                testSplitCat.right.getImpurity());
+        System.out.println("Impurity after split: " + 
+                testSplitCat.impurity_after);
+        System.out.println("Gini: " + testSplitCat.gini);
         
     }
 }
